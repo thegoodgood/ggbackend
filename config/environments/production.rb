e5a01c28c0662d585fe1759cf3fc2f550827e859
Jasterix <ljcalixte@gmail.com>
2019-07-09 16:56:19 -0400

    new gemfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,8 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
+  # config.i18n.fallbacks = true
+  config.i18n.fallbacks = [I18n.default_locale]
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
@@ -80,6 +81,7 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
