1f4f6b4d745a837da7cdfbfa8bdd1c17147b47c7
Jasterix <ljcalixte@gmail.com>
2019-08-08 22:29:23 -0400

    make it better

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,53 +1,53 @@
 require 'byebug'
 class ListsController < ApplicationController
-  skip_before_action :authorized, only: [ :new, :get_lists, :index]
-
-  def new
-    twitter_api = ApiTwitter.new
-    new_list = twitter_api.new_list(name)
-  end
-
-  def index
-    lists = List.all
-    render json: lists
-  end
-
-  def get_lists
-    lists  = List.all
-    lists.destroy_all
-    twitter_api = ApiTwitter.new
-    my_lists = twitter_api.get_lists
-    my_lists["lists"].map do |list|
-          List.create(
-             id_str: list["id_str"],
-             slug: list["slug"],
-             name: list["name"],
-             uri: list["uri"],
-             subscriber_count: list["subscriber_count"],
-             mode: list["mode"],
-             description: list["description "]
-          )
-    end
-    render json: get_lists
-  end
-
-  def add_single_member(list_name, members_array)
-    id = get_list_id(list_name)
-    members_array.map do |member|
-      twitter_api.addSingleMember(id, member)
-    end
-  end
-
-
-  def search_api
-    term = params[:topic]
-    get_list(term)
-  end
-
-  def get_list_id(name)
-    list = List.find_by(id_str: params[:id_str])
-    if list
-      list_id = list.id
-    end
+  # skip_before_action :authorized, only: [ :new, :get_lists, :index]
+  #
+  # def new
+  #   twitter_api = ApiTwitter.new
+  #   new_list = twitter_api.new_list(name)
+  # end
+  #
+  # def index
+  #   lists = List.all
+  #   render json: lists
+  # end
+  #
+  # def get_lists
+  #   lists  = List.all
+  #   lists.destroy_all
+  #   twitter_api = ApiTwitter.new
+  #   my_lists = twitter_api.get_lists
+  #   my_lists["lists"].map do |list|
+  #         List.create(
+  #            id_str: list["id_str"],
+  #            slug: list["slug"],
+  #            name: list["name"],
+  #            uri: list["uri"],
+  #            subscriber_count: list["subscriber_count"],
+  #            mode: list["mode"],
+  #            description: list["description "]
+  #         )
+  #   end
+  #   render json: get_lists
+  # end
+  #
+  # def add_single_member(list_name, members_array)
+  #   id = get_list_id(list_name)
+  #   members_array.map do |member|
+  #     twitter_api.addSingleMember(id, member)
+  #   end
+  # end
+  #
+  #
+  # def search_api
+  #   term = params[:topic]
+  #   get_list(term)
+  # end
+  #
+  # def get_list_id(name)
+  #   list = List.find_by(id_str: params[:id_str])
+  #   if list
+  #     list_id = list.id
+  #   end
 
   end
