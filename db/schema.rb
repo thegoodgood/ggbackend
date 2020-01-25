1c58bc90ed3038856e4e01ccbaab88235b68fce8
Jasterix <ljcalixte@gmail.com>
2020-01-24 20:13:52 -0500

    create Procfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_044816) do
+ActiveRecord::Schema.define(version: 2019_08_17_004538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "source"
+    t.string "author"
+    t.string "title"
+    t.string "description"
+    t.string "url"
+    t.string "urlToImage"
+    t.string "publishedAt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "hashtags", force: :cascade do |t|
     t.string "keyword"
